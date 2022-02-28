a35a96ea64a1a2f1782eaec7d0e12c31f26b0e86
ta211 <55717622+ta211@users.noreply.github.com>
2022-02-28 13:01:51 -0500

    Product page - Quantity Selector - plus & minus js

--- main.js
+++ main.js
@@ -6,3 +6,34 @@ function productBtnGroupUpdate(item) {
     // Select this item
     item.classList.add("selected");
 }
+
+function decrementQuantity(item) {
+    let prev = parseInt(item.parentNode.children[1].value);
+    item.parentNode.children[1].value = Math.max(prev - 1, 1);
+    if (prev - 1 <= 1) {
+        item.parentNode.children[0].classList.add("disabled");
+    } 
+}
+
+function incrementQuantity(item) {
+    let prev = parseInt(item.parentNode.children[1].value);
+    item.parentNode.children[1].value = prev + 1;
+    if (prev == 1) {
+        item.parentNode.children[0].classList.remove("disabled");
+    } 
+}
+
+function updateQuantity(item) {
+    let val = parseInt(item.value);
+    if (val <= 1) {
+        val = 1;
+        item.value = 1;
+    }
+    if (val) {
+        if (val > 1) {
+            item.parentNode.children[0].classList.remove("disabled");
+        } else { // val == 1
+            item.parentNode.children[0].classList.add("disabled");
+        }
+    }
+}
