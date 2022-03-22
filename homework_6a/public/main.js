084ad26e36ea6c2fc535fff1f5a20377154d718e
ta211 <55717622+ta211@users.noreply.github.com>
2022-03-22 02:17:46 -0400

    new Product page; add to cart

--- homework_6a/public/main.js
+++ homework_6a/public/main.js
@@ -1,3 +1,16 @@
+/******************** Classes ********************/
+function CartItem(name, subscribe, option, price, quantity, span, img, link) {
+    this.name = name; // "Protein Shakes"
+    this.subscribe = subscribe; // boolean
+    this.option = option; // "<option name>: <selected option>"
+    this.price = price; // "$4.00"
+    this.quantity = quantity; // integer >= 1
+    this.span = span; // "1 week"
+    this.img = img; // "files/products/drink-1.png"
+    this.link = link; // "products/drinks/drink-1.html"
+}
+
+/******************** Account management ********************/
 function login() {
     let username = document.getElementById("username");
     let password = document.getElementById("password");
@@ -22,6 +35,7 @@ function gotoAccountPage(prefix) {
     }
 }
 
+/******************** Detail page management ********************/
 function productBtnGroupUpdate(item) {
     // De-selected previously selected item
     let selected = item.parentNode.getElementsByClassName("selected");
@@ -59,4 +73,33 @@ function updateQuantity(item) {
             item.parentNode.children[0].classList.add("disabled");
         }
     }
+}
+
+function addToCart(method) {
+    // TODO: Subscribed items should be cheaper for like 10%. Add this in the future.
+    const name = document.getElementsByClassName("product-info-title")[0].textContent;
+    const optionName = document.getElementsByClassName("product-options text")[0].children[0].textContent;
+    const selected = document.getElementsByClassName("single-select selected")[0];
+    const optionSelected = selected.firstChild.textContent.trim();
+    const option = optionName + " " + optionSelected;
+    const price = selected.lastChild.textContent.trim();
+    const quantity = document.getElementById("quantity").value;
+    const span = document.getElementById("delivery-span").value;
+    const img = document.getElementById("product-current-image").getAttribute("src").substring(6);
+    const link = document.location.href;
+
+    let item;
+    if (method == "subscribe") {
+        item = new CartItem(name, true, option, price, quantity, span, img, link);
+    } else if (method == "one-time") {
+        item = new CartItem(name, false, option, price, quantity, span, img, link);
+    }
+
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    if (Array.isArray(cart)) {
+        cart.push(item);
+    } else {
+        cart = [item];
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
 }
