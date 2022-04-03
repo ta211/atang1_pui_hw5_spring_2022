0cf6b90dc76e5c2e6b2325d67045190616a6c8af
ta211 <55717622+ta211@users.noreply.github.com>
2022-04-02 21:56:48 -0400

    Create new branch for HW6A

--- public/main.js
+++ public/main.js
@@ -1,3 +1,17 @@
+/******************** Classes ********************/
+function CartItem(name, subscribe, option, price, quantity, span, img, alt, link) {
+    this.name = name; // "Protein Shakes"
+    this.subscribe = subscribe; // boolean
+    this.option = option; // "<option name>: <selected option>"
+    this.price = price; // "$4.00"
+    this.quantity = quantity; // integer >= 1
+    this.span = span; // "1 week"
+    this.img = img; // "files/products/drink-1.png"
+    this.alt = alt;
+    this.link = link; // "products/drinks/drink-1.html"
+}
+
+/******************** Account management ********************/
 function login() {
     let username = document.getElementById("username");
     let password = document.getElementById("password");
@@ -14,7 +28,7 @@ function logout() {
 
 function gotoAccountPage(prefix) {
     const loggedin = window.localStorage.getItem("logged-in");
-    console.log(loggedin);
+    // console.log(loggedin);
     if (loggedin == "true") {
         window.location.href = prefix + 'account.html';
     } else {
@@ -22,6 +36,7 @@ function gotoAccountPage(prefix) {
     }
 }
 
+/******************** Detail page management ********************/
 function productBtnGroupUpdate(item) {
     // De-selected previously selected item
     let selected = item.parentNode.getElementsByClassName("selected");
@@ -58,5 +73,142 @@ function updateQuantity(item) {
         } else { // val == 1
             item.parentNode.children[0].classList.add("disabled");
         }
+    }
+}
+
+function addToCart(method) {
+    // TODO: Subscribed items should be cheaper for like 10%. Add this in the future.
+    const name = document.getElementsByClassName("product-info-title")[0].textContent;
+    const optionName = document.getElementsByClassName("product-options text")[0].children[0].textContent;
+    const selected = document.getElementsByClassName("single-select selected")[0];
+    const optionSelected = selected.firstChild.textContent.trim();
+    const option = optionName + " " + optionSelected;
+    const price = selected.lastChild.textContent.trim();
+    const quantity = document.getElementById("quantity").value;
+    const span = document.getElementById("delivery-span").value;
+    const img = document.getElementById("product-current-image").getAttribute("src").substring(6);
+    const alt = document.getElementById("product-current-image").getAttribute("alt");
+    const link = document.location.href;
+
+    let item;
+    if (method == "subscribe") {
+        item = new CartItem(name, true, option, price, quantity, span, img, alt, link);
+    } else if (method == "one-time") {
+        item = new CartItem(name, false, option, price, quantity, span, img, alt, link);
+    }
+
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    if (Array.isArray(cart)) {
+        cart.push(item);
+    } else {
+        cart = [item];
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+    loadCartSize();
+
+    window.location.href="../../cart.html";
+}
+
+function loadCartSize() {
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    let cartSize;
+    let element = document.getElementById("cart-icon");
+    if (Array.isArray(cart) && cart.length > 0) {
+        cartSize = cart.length;
+        let sizeNode = document.createElement("div");
+        sizeNode.setAttribute("id", "cart-icon-size");
+        sizeNode.textContent = cartSize;
+        if (element.children.length == 1) {
+            element.appendChild(sizeNode);
+        } else if (element.children.length == 2) {
+            element.replaceChild(sizeNode, element.lastChild);
+        }
+    }
+}
+
+function loadCartContent() {
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    if (Array.isArray(cart)) {
+        // hide the notice
+        if (cart.length > 0) {
+            document.getElementById("empty-notice").classList.add("hidden");
+        }
+        // get the template
+        let cartItemTemplate = document.getElementById("cart-item-template");
+        // clear the cart inner container (reload)
+        document.getElementById("cart-items-inner").innerHTML="";
+        // load each item into the template
+        let totalPrice = 0;
+        for (let entry of cart.entries()) {
+            let i = entry[0];
+            let item = entry[1];
+            // console.log(item);
+            
+            let cartItem = cartItemTemplate.cloneNode(true);
+            cartItem.setAttribute("id", "cart-item-"+i);
+            // console.log(cartItem);
+            // Set up item image
+            let imgNode = document.createElement("img");
+            imgNode.setAttribute("src", item.img);
+            imgNode.setAttribute("alt", item.alt);
+            let aNode = document.createElement("a");
+            aNode.setAttribute("href", item.link);
+            aNode.appendChild(imgNode);
+            cartItem.getElementsByClassName("cart-item-img")[0].appendChild(aNode);
+            // Set up item info - name & options
+            cartItem.getElementsByClassName("item-name")[0].textContent = item.name;
+            cartItem.getElementsByClassName("item-option")[0].textContent = item.option;
+            // Set up item price
+            cartItem.getElementsByClassName("price")[0].textContent = item.price;
+
+            let itemPrice = Number.parseFloat(item.price.substring(1)) * item.quantity;
+            if (!isNaN(itemPrice)) {
+                totalPrice += itemPrice;
+            }
+            // Set up checkbox
+            let checkbox = cartItem.getElementsByClassName("subscription-checkbox")[0].getElementsByTagName("input")[0];
+            checkbox.setAttribute("id", "subscription-"+i);
+            checkbox.setAttribute("name", "subscription-"+i);
+            checkbox.checked=item.subscribe;
+            if (item.subscribe) {
+                checkbox.setAttribute("class", "checked");
+            } else {
+                checkbox.setAttribute("class", "");
+            }
+            // Set up quantity
+            let quantitySelector = cartItem.getElementsByClassName("quantity-selector")[0];
+            quantitySelector.getElementsByTagName("label")[0].setAttribute("for", "quantity-"+i);
+            quantitySelector.getElementsByClassName("quantity-minus")[0].setAttribute("data-field", "quantity-"+i);
+            quantitySelector.getElementsByClassName("quantity-plus")[0].setAttribute("data-field", "quantity-"+i);
+            let quantityField = quantitySelector.getElementsByClassName("quantity-field")[0];
+            quantityField.setAttribute("id", "quantity-"+i);
+            quantityField.setAttribute("name", "quantity-"+i);
+            quantityField.value = item.quantity;
+            updateQuantity(quantityField);
+            // Set up delivery
+            let spanSelector = cartItem.getElementsByTagName("select")[0];
+            spanSelector.setAttribute("id", "delivery-span-"+i);
+            spanSelector.setAttribute("name", "delivery-span-"+i);
+            spanSelector.value = item.span;
+            // Set up removal method
+            cartItem.getElementsByClassName("cart-item-removal-method")[0].setAttribute("onclick", "removeCartItem("+i+");")
+            // Append item to cart-items-inner
+            // console.log(cartItem);
+            document.getElementById("cart-items-inner").appendChild(cartItem);
+        }
+        document.getElementById("cart-total-price").textContent = "Total: $" + totalPrice.toFixed(2);
+    } else {
+        document.getElementById("empty-notice").removeAttribute("class", "hidden");
+    }
+}
+
+function removeCartItem(index) {
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    let removedItem = cart.splice(index,1)[0];
+    let isExecuted = confirm("Are you sure you want to remove " + removedItem.name + " - " + removedItem.option + " x " + removedItem.quantity + " ?");
+    if (isExecuted) {
+        localStorage.setItem("cart", JSON.stringify(cart));
+        loadCartSize();
+        window.location.reload();
     }
 }
